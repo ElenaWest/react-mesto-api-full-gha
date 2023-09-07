3926262a62c1a46565877ef7a64f777b855cae29
Elena Vasilenko <elenawest92@yandex.ru>
2023-09-07 14:34:52 +0300

    Исправить ошибки по сле первого ревью

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -3,6 +3,9 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
+
+const { SECRET_KEY } = process.env;
+const { NODE_ENV } = process.env;
 const BadRequestError = require('../errors/BadRequestError');
 const NotFoundError = require('../errors/NotFoundError');
 const ConflictError = require('../errors/ConflictError');
@@ -87,7 +90,7 @@ module.exports.login = (req, res, next) => {
   const { email, password } = req.body;
   return User.findUserByCredentials(email, password)
     .then((user) => {
-      const token = jwt.sign({ _id: user._id }, 'hello', { expiresIn: '7d' });
+      const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? SECRET_KEY : 'hello', { expiresIn: '7d' });
       res.send({ token });
     })
     .catch((error) => {
